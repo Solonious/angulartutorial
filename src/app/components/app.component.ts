ffbf6dc82b44e63783a47a3abfa50d226b2db642
sergey.solonar <sergey.solonar@dukascopy.com>
2017-07-20 18:46:35 +0300

    relocate header component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <section class="fluid-container">
-            <app-header></app-header>
-            <router-outlet></router-outlet>
+        <section class="container-fluid">
+                <!--<app-header></app-header>-->
+                <!--<nav>-->
+                <!--<a routerLink="/test" routerLinkActive="active">test</a>-->
+                <!--</nav>-->
+                <router-outlet></router-outlet>
         </section>
     `,
     styleUrls: ['./app.component.css']
